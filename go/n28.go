9da09615fd7368c8530fe3886b77d44e12a14dcc
Shibata Kazuki <kshibata101@gmail.com>
2019-03-10 16:19:20 +0900

    No.28

---
+++ go/n28.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "os"
+    "bufio"
+    "regexp"
+    "fmt"
+    "strings"
+)
+
+func main() {
+    texts := []string{}
+
+    r := regexp.MustCompile(`^\|(.*)`)
+    rs := regexp.MustCompile(`^\{\{基礎情報`)
+    re := regexp.MustCompile(`^\}\}`)
+    
+    filepath := "jawiki-uk.txt"
+    f, _ := os.Open(filepath)
+    defer f.Close()
+
+    basicInfo := false
+    s := bufio.NewScanner(f)
+    for s.Scan() {
+        l := s.Text()
+        
+        if re.Match([]byte(l)) {
+            basicInfo = false
+        }
+        if basicInfo {
+            res := r.FindStringSubmatch(l)
+            if res == nil {
+                texts[len(texts)-1] += l
+            } else {
+                texts = append(texts, res[1])
+            }
+        }
+        if rs.Match([]byte(l)) {
+            basicInfo = true
+        }
+    }
+
+    rst := regexp.MustCompile(`'{2,}`)
+    rmrk := regexp.MustCompile(`\[\[(([^\[]*)\|)?([^\[]*?)\]\]`)
+    rhttp := regexp.MustCompile(`\[(.*?)\]`)
+    rtpl := regexp.MustCompile(`\{\{([^\}]*\|)?([^\}]*?)\}\}`)
+    rstar := regexp.MustCompile(`\*`)
+    
+    m := make(map[string]string)
+    for _, text := range texts {
+        res := strings.Split(text, " = ")
+        if (len(res) == 2) {
+            word := rst.ReplaceAllString(res[1], "")
+            word = rmrk.ReplaceAllString(word, "$3")
+            word = rhttp.ReplaceAllString(word, "$1")
+            word = rtpl.ReplaceAllString(word, "$2")
+            word = rstar.ReplaceAllString(word, "")
+            m[res[0]] = word
+        }
+    }
+
+    fmt.Println(m)
+}
