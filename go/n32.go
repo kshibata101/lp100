e76049514d17f756e6f5d3e6271c1ce97428016b
Shibata Kazuki <kshibata101@gmail.com>
2019-03-17 16:31:56 +0900

    No.32

---
+++ go/n32.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "./morpheme"
+    "fmt"
+)
+
+func main() {
+    states := morpheme.Load("../neko.txt.mecab")
+    for _, state := range states {
+        for _, morph := range state {
+            if morph.Pos == "動詞" {
+                fmt.Println(morph.Base)
+            }
+        }
+    }
+}
