f9c76581ed23cf4221cfcd0c4567259f7cfd0aef
Shibata Kazuki <kshibata101@gmail.com>
2019-04-08 00:50:44 +0900

    fix No.45 to apply to all texts

--- go/n45.go
+++ go/n45.go
@@ -8,20 +8,21 @@ import (
 
 func main() {
     sentences := cabocha.LoadAsChunk("../neko.txt.cabocha")
-    sentence := sentences[5]
-    for _, chunk := range sentence {
-        for _, morph := range chunk.Morphs {
-            if morph.Pos == "動詞" {
-                kaku := []string{}
-                for _, src := range chunk.Srcs {
-                    prevChunk := sentence[src]
-                    for _, prevMorph := range prevChunk.Morphs {
-                        if prevMorph.Pos == "助詞" {
-                            kaku = append(kaku, prevMorph.Base)
+    for _, sentence := range sentences {
+        for _, chunk := range sentence {
+            for _, morph := range chunk.Morphs {
+                if morph.Pos == "動詞" {
+                    kaku := []string{}
+                    for _, src := range chunk.Srcs {
+                        prevChunk := sentence[src]
+                        for _, prevMorph := range prevChunk.Morphs {
+                            if prevMorph.Pos == "助詞" {
+                                kaku = append(kaku, prevMorph.Base)
+                            }
                         }
                     }
+                    fmt.Println(morph.Base + "\t" + strings.Join(kaku, " "))
                 }
-                fmt.Println(morph.Base + "\t" + strings.Join(kaku, " "))
             }
         }
     }
