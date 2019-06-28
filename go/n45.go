ccf5f852f77bba13c198d21c859b80e0637c20db
Shibata Kazuki <kshibata101@gmail.com>
2019-06-29 00:55:46 +0900

    fix to use tab

--- go/n45.go
+++ go/n45.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "./cabocha"
-    "fmt"
-    "strings"
+	"./cabocha"
+	"fmt"
+	"strings"
 )
 
 func main() {
-    sentences := cabocha.LoadAsChunk("../neko.txt.cabocha")
-    for _, sentence := range sentences {
-        for _, chunk := range sentence {
-            for _, morph := range chunk.Morphs {
-                if morph.Pos == "動詞" {
-                    kaku := []string{}
-                    for _, src := range chunk.Srcs {
-                        prevChunk := sentence[src]
-                        for _, prevMorph := range prevChunk.Morphs {
-                            if prevMorph.Pos == "助詞" {
-                                kaku = append(kaku, prevMorph.Base)
-                            }
-                        }
-                    }
-                    fmt.Println(morph.Base + "\t" + strings.Join(kaku, " "))
-                }
-            }
-        }
-    }
+	sentences := cabocha.LoadAsChunk("../neko.txt.cabocha")
+	for _, sentence := range sentences {
+		for _, chunk := range sentence {
+			for _, morph := range chunk.Morphs {
+				if morph.Pos == "動詞" {
+					kaku := []string{}
+					for _, src := range chunk.Srcs {
+						prevChunk := sentence[src]
+						for _, prevMorph := range prevChunk.Morphs {
+							if prevMorph.Pos == "助詞" {
+								kaku = append(kaku, prevMorph.Base)
+							}
+						}
+					}
+					fmt.Println(morph.Base + "\t" + strings.Join(kaku, " "))
+				}
+			}
+		}
+	}
 }
