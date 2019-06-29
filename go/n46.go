cf730df7917c6eaf661eb8d9212e07771cf2b0ed
Shibata Kazuki <kshibata101@gmail.com>
2019-06-29 22:51:51 +0900

    add No.46

---
+++ go/n46.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"./cabocha"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	sentences := cabocha.LoadAsChunk("../neko.txt.cabocha")
+	for _, sentence := range sentences {
+		for _, chunk := range sentence {
+			for _, morph := range chunk.Morphs {
+				if morph.Pos == "動詞" {
+					kaku := []string{}
+					pattern := []string{}
+					for _, src := range chunk.Srcs {
+						prevChunk := sentence[src]
+						for _, prevMorph := range prevChunk.Morphs {
+							if prevMorph.Pos == "助詞" {
+								kaku = append(kaku, prevMorph.Base)
+								pattern = append(pattern, cabocha.GetChunkSurface(prevChunk))
+							}
+						}
+					}
+					fmt.Println(morph.Base + "\t" + strings.Join(kaku, " ") + "\t" + strings.Join(pattern, " "))
+				}
+			}
+		}
+	}
+}
